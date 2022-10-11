1aef683db5b2828849a8b623f2021428a021d6e8
someshjadhwani24 <somesh.jadhwani104@nmims.edu.in>
2022-10-11 12:51:13 +0530

    rev 1

---
+++ app/user.java
@@ -0,0 +1,22 @@
+public class user {
+    String user_name;
+    String user_id;
+    String password;
+    String email;
+    long contact;
+    boolean logged_in;
+
+    user(String name, String id,String pass,String email,long contact,boolean logged){
+        this.user_name = name;
+        this.contact = contact;
+        this.user_id = id;
+        this.password = pass;
+        this.email = email;
+        this.logged_in = logged;
+
+    }
+
+
+
+
+}
